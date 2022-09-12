2a933808511ef7623d83cc80f04aeb6a3b2b2487
favour-design <favourmje@gmail.com>
2022-09-11 17:04:58 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
-int main(void) {
+/**
+ * main  print
+ *
+ * Return: Always 0 (its success)
+ */
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle\n");
-	return (0);}
+	return (0);
+}
