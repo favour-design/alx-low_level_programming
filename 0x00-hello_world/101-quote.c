65e69b2ffa26c6973d24495bab798f75ae6b0d16
favour-design <favourmje@gmail.com>
2022-09-12 21:08:05 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: Print exactly
+ *
+ * Return:Always 1 (sucessful)
+ */
+
+
+int main(void)
+{
+	 
+	write(2, "and that piece of art is useful\"-Dora Kerpar, 2015-10-19\n", 61);
+	return (1);
+}
+
+
