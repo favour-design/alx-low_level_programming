17be26efb77ae0e5bc18fb5762ccf147ad81d119
favour-design <favourmje@gmail.com>
2022-09-12 17:32:11 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * Main - Entry point
  *
  * Discription: A script that prints
  *
  * Return: Always 0 (its success)
  */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
+
 }
