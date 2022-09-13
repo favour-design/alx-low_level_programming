75e15f1a2c8b1685495e941f494cc5539d56ee90
favour-design <favourmje@gmail.com>
2022-09-12 17:16:47 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription:Using printf
+ *
+ * Return:Always 0 (successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
