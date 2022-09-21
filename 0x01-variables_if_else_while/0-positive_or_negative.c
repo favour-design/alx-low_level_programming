2b231b5f227c7b8c2a3e765ce86760d8783fa57b
favour-design <favourmje@gmail.com>
2022-09-20 20:10:31 -0700

    fix betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - print if number is positive or negative 
+ * main - Print positive or negative number
+ *
+ * Discription : To check number
  *
  * Return : Always (successful)
  */
@@ -11,6 +13,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
