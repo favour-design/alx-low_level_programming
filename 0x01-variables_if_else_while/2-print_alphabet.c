5a534cbf4aeead522cf7765885b620c391781428
favour-design <favourmje@gmail.com>
2022-09-21 19:23:58 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription : Using putchar only
+ *
+ * Return: Always (successful)
+ */
+
+int main(void)
+{
+	int n;
+	char zh = 'a';
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	while (zh < 'z')
+	{
+		putchar(zh);
+		++zh;
+	}
+	putchar ('\n');
+	return (0);
+}
+
