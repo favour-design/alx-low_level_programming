e7998f7cacc24f3cfdd1dbb87dcb1de416f62f1f
favour-design <favourmje@gmail.com>
2022-09-24 15:49:43 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Discription: print alphabets
+ *
+ * Return: Always (successful)
+ */
+
+int main(void)
+{
+	char d;
+
+	d = 'a';
+
+	while (d <= 'z')
+	{
+	if (d == 'e' || d == 'q');
+		++d;
+
+		{
+			putchar (d);
+		}
+	}
+	putchar ('\n');
+	
+	return (0);
+}
+
+
+
+
+
+
+
