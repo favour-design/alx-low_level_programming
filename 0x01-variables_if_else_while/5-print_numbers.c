20893ca98e091e09cd776d1b64570d580c9ae218
favour-design <favourmje@gmail.com>
2022-09-24 17:19:13 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Discription: Print numbers 0-10
+ *
+ * Return: Always (successful)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 0; n <= 9; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
